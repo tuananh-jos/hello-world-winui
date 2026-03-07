72dd3fbd0a518b3f80222dfb7ecc4db6b1cb1db5
tuananh.jos <tuananh.jos@gmail.com>
2026-03-07 22:53:20 +0700

    feat: fix Warning Nullable Reference Types of models entity

--- App7.Domain/Entities/Model.cs
+++ App7.Domain/Entities/Model.cs
@@ -1,37 +1,19 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace App7.Domain.Entities;
+
 public class Model
 {
-    public Guid Id
-    {
-        get; set;
-    }
-    public string Name
-    {
-        get; set;
-    }
-    public string Manufacturer
-    {
-        get; set;
-    }
-    public string Category
-    {
-        get; set;
-    }
-    public string SubCategory
-    {
-        get; set;
-    }
-    public int Available
-    {
-        get; set;
-    }
-
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Manufacturer { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
+    public string SubCategory { get; set; } = string.Empty;
+    public int Available { get; set; } = 0;
     /// <summary>True when there is at least one device available to borrow.</summary>
     public bool IsAvailable => Available > 0;
 }
